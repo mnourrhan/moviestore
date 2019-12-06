79d4b77df0e39af3d1fbfb804d47a9a8c6e7ea99
mnourrhan <mnourrhan@gmail.com>
2019-12-06 18:57:01 +0200

    Work Done: ------------ - Integrate with device package. - Add login and register form. - Add routes nedded for user login, register and logout. - Add database_authenticatable, registerable,.. device modules to user model. - Add device's user table to database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'movies#index'
   resources :movies
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
